a86a818a842d024f8b4572f6f901e0f09c7e6d66
dinhodoes <jahardy34@gmail.com>
2014-09-08 20:19:16 -0700

    I DID IT

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @user = User.new
-    @is_signup = true
-    # redirect_to users_path if current_user
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-
-  get 'welcome/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
 
   # Home
-  root 'welcome#index'
+  root 'trips#index'
 
   # User Routing
   post 'users/new' => 'users#create', as: :new_user
