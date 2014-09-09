faf0a14baf085a976e3441bf83c370b487f64d75
dinhodoes <jahardy34@gmail.com>
2014-09-08 18:50:21 -0700

    welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
 
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
 
   # Home
-  root 'trips#index'
+  root 'welcome#index'
 
   # User Routing
   post 'users/new' => 'users#create', as: :new_user
