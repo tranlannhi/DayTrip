0fa6bdf94c7a54be89bd6be7ca8fb5dc793c2851
dinhodoes <jahardy34@gmail.com>
2014-09-08 19:58:57 -0700

    trips contl

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,10 +1,10 @@
 class TripsController < ApplicationController
 
 	def create
-    @trip = Trip.new(params.require(:trip).permit(:origin, :destination, :searchString, :id))
+    @trip = Trip.new(params.require(:trip).permit(:origin, :destination, :searchString, :waypoints))
 	  	if @trip.save
 		  	redirect_to new_trip_path
-		  	puts Trip.last
+		  	# puts Trip.last
 	    else
 	      render 'new'
 	  	end
@@ -12,7 +12,7 @@ class TripsController < ApplicationController
 
   def index
   	# @trips = Trip.all
-	  @trip = Trip.new
+	  # @trip = Trip.new(params.require(:trip).permit(:origin, :destination, :searchString, :waypoints))
 	end
 
 	def new
