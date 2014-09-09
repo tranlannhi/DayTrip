b6a7c27477abf014be79da46c2725dc4a0e29eed
dinhodoes <jahardy34@gmail.com>
2014-09-08 17:10:47 -0700

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   # Home
-  root 'trips#index'
+  get 'trips/index' => 'trips#index'
 
   # User Routing
   post 'users/new' => 'users#create', as: :new_user
