8d4dc7d355c638277b068049e79324038a63052b
Chibird12 <kaythxs@gmail.com>
2014-09-09 17:36:44 -0700

    sdsd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-  
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
